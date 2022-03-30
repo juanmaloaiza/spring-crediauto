2c8d8eb8a612ebc2340af6ecd99b8dbc8d70fc98
Daniel Jácome <edjacome@pichincha.com>
2022-03-30 18:31:14 -0500

    first commmit

---
+++ src/main/java/com/bancopichincha/credito/automotriz/CaMsaCreditoautoApplication.java
@@ -0,0 +1,13 @@
+package com.bancopichincha.credito.automotriz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaMsaCreditoautoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaMsaCreditoautoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bancopichincha/credito/automotriz/CaMsaCreditoautoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancopichincha.credito.automotriz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaMsaCreditoautoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
